8cb3805b91a5dd251d073b6b71ea2dd4a5d05643
Eric <eandrews737@gmail.com>
2019-05-16 16:16:09 -0700

    prettier/import api token

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
-import WeatherStatusComponent from './Components/weather-status-component';
+import React from "react";
+import "./App.css";
+import WeatherStatusComponent from "./Components/weather-status-component";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <WeatherStatusComponent/>
+        <WeatherStatusComponent />
       </header>
     </div>
   );
--- src/util/dark-sky-api-util.js
+++ src/util/dark-sky-api-util.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { DARK_SKY_API_KEY } from "../env.js";
 
 export function getWeatherByCoordinates(coordinates = "42.3601,-71.0589") {
   return axios
     .get(
-      `https://cors.io/?https://api.darksky.net/forecast/409df4b9cbf9886dd035ff029dd7063b/${coordinates}`
+      `https://cors.io/?https://api.darksky.net/forecast/${DARK_SKY_API_KEY}/${coordinates}`
     )
     .then(Response => {
       return Response.data;
