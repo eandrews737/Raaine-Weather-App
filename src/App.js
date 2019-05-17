12a32fc2708e213059674f1929942cf9e3243b36
Eric <eandrews737@gmail.com>
2019-05-17 13:15:58 -0700

    better routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import WeatherStatusComponent from "./Components/weather-status-component";
+import LocationSearchComponent from "./Components/location-search-component";
+import HeaderComponent from "./Components/header-component";
+import FooterComponent from "./Components/footer-component";
+import NotfoundComponent from "./Components/not-found-component";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <WeatherStatusComponent />
-      </header>
+      <HeaderComponent />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LocationSearchComponent} />
+          <Route path="/search" component={WeatherStatusComponent} />
+          <Route component={NotfoundComponent} />
+        </Switch>
+      </Router>
+      <FooterComponent />
     </div>
   );
 }
