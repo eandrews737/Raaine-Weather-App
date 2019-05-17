e806875c5cc56748b39fb20e90cabe0a53f94c97
Eric <eandrews737@gmail.com>
2019-05-17 16:06:10 -0700

    cleanup

--- src/Components/location-search-component.js
+++ src/Components/location-search-component.js
@@ -2,13 +2,26 @@ import React from "react";
 import { geolocated } from "react-geolocated";
 
 class LocationSearchComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: false
+    };
+  }
+
   render() {
     return (
       <div>
         <div>Search by City</div>
         <input />
         <div>or</div>
-        <button>Allow Location</button>
+        <button
+          onClick={() => {
+            this.setState({ redirect: true });
+          }}
+        >
+          Allow Location
+        </button>
       </div>
     );
   }
