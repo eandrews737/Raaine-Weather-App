8c5c135e5ab3be58d10bbdd00a43a38ce0e153f8
Eric <eandrews737@gmail.com>
2019-05-17 13:16:27 -0700

    header and footer components

---
+++ src/Components/footer-component.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class FooterComponent extends React.Component {
+  render() {
+    return <div className="App-footer" />;
+  }
+}
+
+export default FooterComponent;
---
+++ src/Components/header-component.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class HeaderComponent extends React.Component {
+  render() {
+    return <div className="App-header" />;
+  }
+}
+
+export default HeaderComponent;
