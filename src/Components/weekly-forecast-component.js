4d998c1749fd74eb08a25c1cc01831e10dc7f9c6
Eric <eandrews737@gmail.com>
2019-05-22 17:22:09 -0700

    Weekly summary in table / new app name

--- src/Components/weekly-forecast-component.js
+++ src/Components/weekly-forecast-component.js
@@ -1,9 +1,49 @@
 import React from "react";
 import { connect } from "react-redux";
-import { withRouter } from "react-router";
 import * as moment from "moment";
+import { withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+// Allows the tablecell to be customized a bit further
+const CustomTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white
+  },
+  body: {
+    fontSize: 14
+  }
+}))(TableCell);
+
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 700
+  },
+  row: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.background.default
+    }
+  }
+});
 
 class WeeklyForecastComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFahrenheit: true
+    };
+  }
+
   // returns day of the week by name
   dayToString(dayByNumber) {
     switch (dayByNumber) {
@@ -21,7 +61,17 @@ class WeeklyForecastComponent extends React.Component {
         return "Fri";
       case 6:
         return "Sat";
+      case 7:
+        return "Sun";
     }
+  }
+
+  // returns temperature based on selected scale
+  temperatureConversion(temperature) {
+    const { isFahrenheit } = this.state;
+
+    if (isFahrenheit) return temperature;
+    else return temperature * (5 / 9) - 32;
   }
 
   // returns icon by status
@@ -30,36 +80,55 @@ class WeeklyForecastComponent extends React.Component {
   }
 
   render() {
-    const { darkSkyJson } = this.props;
+    const { classes, darkSkyJson } = this.props;
     let sevenDayForecast = [];
 
     // tomorrow - a week today
     if (darkSkyJson.daily)
-      sevenDayForecast = darkSkyJson.daily.data.slice(1, 7);
+      sevenDayForecast = darkSkyJson.daily.data.slice(1, 8);
 
     return (
-      <div style={{ paddingBottom: "100px" }}>
-        <h2>Seven Day Forecast</h2>
-        <div>
-          {darkSkyJson.daily &&
-            sevenDayForecast.map(day => (
-              <div className="week-day-box">
-                <div>
-                  <div>
-                    {this.dayToString(moment.unix(day.time).isoWeekday())}
-                  </div>
-                  <div>{moment.unix(day.time).format("MMM DD")}</div>
-                  <div>insert icon: {day.icon}</div>
-                </div>
-                <div>{day.summary}</div>
-                <div>
-                  {day.temperatureHigh}/{day.temperatureLow}
-                </div>
-                <div>{day.precipProbability * 100}%</div>
-              </div>
-            ))}
-        </div>
-      </div>
+      <Paper className={classes.root}>
+        {darkSkyJson.daily && (
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <CustomTableCell>Day</CustomTableCell>
+                <CustomTableCell align="right">Description</CustomTableCell>
+                <CustomTableCell align="right">High/Low</CustomTableCell>
+                <CustomTableCell align="right">Chance of Rain</CustomTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {sevenDayForecast.map((day, index) => (
+                <TableRow className={classes.row} key={index}>
+                  <CustomTableCell component="th" scope="row">
+                    <div>
+                      <div>
+                        {this.dayToString(moment.unix(day.time).isoWeekday())}
+                      </div>
+                      <div>{moment.unix(day.time).format("MMM DD")}</div>
+                      <div>insert icon: {day.icon}</div>
+                    </div>
+                  </CustomTableCell>
+                  <CustomTableCell align="right">{day.summary}</CustomTableCell>
+                  <CustomTableCell align="right">
+                    {Math.round(
+                      this.temperatureConversion(day.temperatureHigh)
+                    )}
+                    {`\xB0`}/
+                    {Math.round(this.temperatureConversion(day.temperatureLow))}
+                    {`\xB0`}
+                  </CustomTableCell>
+                  <CustomTableCell align="right">
+                    {Math.round(day.precipProbability * 100)}%
+                  </CustomTableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
+      </Paper>
     );
   }
 }
@@ -68,4 +137,6 @@ const mapStateToProps = state => ({
   darkSkyJson: state.darkSkyJson
 });
 
-export default connect(mapStateToProps)(withRouter(WeeklyForecastComponent));
+export default connect(mapStateToProps)(
+  withStyles(styles)(WeeklyForecastComponent)
+);
