1db042e1f04435016583681355665aea350ccada
Eric <eandrews737@gmail.com>
2019-05-22 23:58:39 -0700

    Fixed up error page

--- src/Components/not-found-component.js
+++ src/Components/not-found-component.js
@@ -1,8 +1,14 @@
 import React from "react";
+import errorIcon from "../assets/svg/404-error.svg";
 
 class NotFoundComponent extends React.Component {
   render() {
-    return <div>404 NOT FOUND</div>;
+    return (
+      <div>
+        <img style={{ height: "350px", paddingTop: "10%" }} src={errorIcon} />
+        <h3>Go back traveler, you've gone too far</h3>
+      </div>
+    );
   }
 }
 
