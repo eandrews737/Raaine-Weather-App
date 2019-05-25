1d56eb824f1f00e29c5bf585fa3f78cdd6925ded
Eric <eandrews737@gmail.com>
2019-05-25 01:07:31 -0700

    stripes at bottom

--- src/Components/footer-component.js
+++ src/Components/footer-component.js
@@ -6,7 +6,10 @@ class FooterComponent extends React.Component {
   render() {
     return (
       <div id="footer" className="App-footer">
-        Powered by Dark Sky API
+        <div style={{ backgroundColor: "#ed5121" }} className="footer-stripe" />
+        <div style={{ backgroundColor: "#f9a500" }} className="footer-stripe" />
+        <div style={{ backgroundColor: "#00b6ef" }} className="footer-stripe" />
+        <div style={{ backgroundColor: "#6dd5f4" }} className="footer-stripe" />
       </div>
     );
   }
