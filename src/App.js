589177799235f0376704f3eb417839ad5ff63b78
Eric <eandrews737@gmail.com>
2019-05-26 10:18:10 -0700

    hashrouting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import WeatherPageComponent from "./Components/weather-page-component";
-import LocationSearchComponent from "./Components/location-search-component";
 import HeaderComponent from "./Components/header-component";
 import FooterComponent from "./Components/footer-component";
 import NotfoundComponent from "./Components/not-found-component";
@@ -25,7 +24,7 @@ function App() {
   return (
     <div className="App">
       <MuiThemeProvider theme={theme}>
-        <Router>
+        <Router basename="/">
           <HeaderComponent />
           <Switch>
             <Route exact path="/" component={WeatherPageComponent} />
